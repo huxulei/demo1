1ce273d5105ed544ebe4d279edfbfa7a519cceb5
huxulei <huxule@hotmail.com>
2019-01-01 11:11:55 +0800

    first commit

---
+++ src/com/huxl/base/TestFirst.java
@@ -0,0 +1,16 @@
+package com.huxl.base;
+
+public class TestFirst {
+
+	public static void main(String[] args) {
+		boolean flag = true;
+		int i =0;
+		while (flag) {
+			System.out.println("therd " + i);
+			i ++;
+			if(i>=10) {
+				flag = false;
+			}
+		}
+	}
+}
